74f5dba36612ec6a6a9e31a00df556a40c7cb2d0
gusttavvo <gusta.gustavocruz@gmail.com>
2026-06-18 15:04:03 -0300

    feat: create record AlunoRequest

---
+++ src/main/java/dev/gustavocruz/sysacademy/domain/dtos/AlunoRequest.java
@@ -0,0 +1,43 @@
+package dev.gustavocruz.sysacademy.domain.dtos;
+
+import dev.gustavocruz.sysacademy.domain.Aluno;
+
+import java.time.LocalDate;
+
+public record AlunoRequest(
+        String nome,
+        LocalDate dataNascimento,
+        String genero,
+        String telefone,
+        String celular,
+        String email,
+        String observacao,
+        String endereco,
+        String complemento,
+        String bairro,
+        String cidade,
+        String estado,
+        String cep) {
+
+    public Aluno toEntity() {
+        Aluno aluno = new Aluno();
+        preencher(aluno);
+        return aluno;
+    }
+
+    public void preencher(Aluno aluno) {
+        aluno.setNome(nome);
+        aluno.setDataNascimento(dataNascimento);
+        aluno.setGenero(genero);
+        aluno.setTelefone(telefone);
+        aluno.setCelular(celular);
+        aluno.setEmail(email);
+        aluno.setObservacao(observacao);
+        aluno.setEndereco(endereco);
+        aluno.setComplemento(complemento);
+        aluno.setBairro(bairro);
+        aluno.setCidade(cidade);
+        aluno.setEstado(estado);
+        aluno.setCep(cep);
+    }
+}
