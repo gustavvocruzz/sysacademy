1551384293b58073a7728642fbb1fd0f01f78194
gusttavvo <gusta.gustavocruz@gmail.com>
2026-06-25 14:48:14 -0300

    feat: create RegraDeNegocioException

---
+++ src/main/java/dev/gustavocruz/sysacademy/exception/RegraDeNegocioException.java
@@ -0,0 +1,8 @@
+package dev.gustavocruz.sysacademy.exception;
+
+public class RegraDeNegocioException extends RuntimeException {
+
+    public RegraDeNegocioException(String message){
+        super(message);
+    }
+}
